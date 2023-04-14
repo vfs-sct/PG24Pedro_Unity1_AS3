bd5f9fd3012f15d03b46b24bb0b8930fb84ebe72
pg24pedro <122394677+pg24pedro@users.noreply.github.com>
2023-04-14 11:07:28 -0700

    UPdate

--- PG24Pedro_Unity1_AS3/Assets/Scripts/ShipController.cs
+++ PG24Pedro_Unity1_AS3/Assets/Scripts/ShipController.cs
@@ -13,6 +13,7 @@ public class ShipController : MonoBehaviour
     
     private float yaw;
     private float pitch;
+    private float roll;
     void Start()
     {
         
@@ -27,9 +28,10 @@ public class ShipController : MonoBehaviour
         
         
         yaw += horizontalInput * YawAmount * Time.deltaTime;
-         pitch = Mathf.Lerp(0, 20, Math.Abs(verticalInput)) * Mathf.Sign(verticalInput);
+        pitch = Mathf.Lerp(0, 20, Math.Abs(verticalInput)) * Mathf.Sign(verticalInput);
+        roll = Mathf.Lerp(0, 30, Mathf.Abs(horizontalInput)) * -Mathf.Sign(horizontalInput);
         
-        transform.localRotation = Quaternion.Euler(Vector3.up * yaw + Vector3.right * pitch);
+        transform.localRotation = Quaternion.Euler(Vector3.up * yaw + Vector3.right * pitch + Vector3.forward * roll);
 
     }
 }
